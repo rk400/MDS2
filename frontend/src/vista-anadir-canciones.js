1294d77ef96e46ecb52833ca7848fd41dbc2b07b
CarlosCarrion98 <c.carrionmartinez1998@gmail.com>
2022-03-28 14:49:37 +0200

    Añadido el botón eliminar que faltaba en algunas vistas

--- frontend/src/vista-anadir-canciones.js
+++ frontend/src/vista-anadir-canciones.js
@@ -1,10 +1,10 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
-import '@vaadin/vaadin-button/src/vaadin-button.js';
 import '@polymer/iron-icon/iron-icon.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/vaadin-combo-box/src/vaadin-combo-box.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 class VistaAnadirCanciones extends PolymerElement {
 
@@ -16,14 +16,14 @@ class VistaAnadirCanciones extends PolymerElement {
                     height: 100%;
                 }
             </style>
-<vaadin-vertical-layout style="width: 100%; height: 100%;">
+<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: flex-start; margin-left: var(--lumo-space-s);">
  <h1>Añadir canciones</h1>
  <vaadin-text-field label="Título:" placeholder="Introduzca nombre de la canción" style="width: 60%;"></vaadin-text-field>
  <h2>Subir archivo de audio</h2>
  <vaadin-horizontal-layout theme="spacing" style="width: 100%;">
   <vaadin-text-field style="width: 70%; align-self: center;" label="*Solo permitido: .mp3, .mp4 y .ogg"></vaadin-text-field>
   <vaadin-button style="align-self: flex-end;">
-   Explorar...
+    Explorar... 
   </vaadin-button>
  </vaadin-horizontal-layout>
  <vaadin-horizontal-layout theme="spacing">
@@ -40,9 +40,9 @@ class VistaAnadirCanciones extends PolymerElement {
   </vaadin-button>
  </vaadin-horizontal-layout>
  <vaadin-horizontal-layout theme="spacing">
-  <label style="align-self: center;">álbumes que la incluyen:</label>
+  <label style="align-self: center;">Álbumes que la incluyen:</label>
   <vaadin-combo-box placeholder="Seleccionar álbumes">
-   Seleccionar álbumes
+    Seleccionar álbumes 
   </vaadin-combo-box>
   <vaadin-button theme="icon" aria-label="Add new" style="align-self: center;">
    <iron-icon icon="lumo:plus"></iron-icon>
@@ -52,7 +52,7 @@ class VistaAnadirCanciones extends PolymerElement {
  <vaadin-horizontal-layout theme="spacing">
   <label style="align-self: center;">Productor:</label>
   <vaadin-text-field placeholder="Introduzca productores">
-   Text
+    Text 
   </vaadin-text-field>
  </vaadin-horizontal-layout>
  <vaadin-horizontal-layout theme="spacing">
@@ -65,10 +65,13 @@ class VistaAnadirCanciones extends PolymerElement {
  </vaadin-horizontal-layout>
  <vaadin-horizontal-layout style="align-self: stretch; justify-content: space-around; padding: var(--lumo-space-m);">
   <vaadin-button>
-   Cancelar
+    Cancelar 
+  </vaadin-button>
+  <vaadin-button id="vaadinButton">
+   Eliminar
   </vaadin-button>
   <vaadin-button>
-   Añadir
+    Añadir 
   </vaadin-button>
  </vaadin-horizontal-layout>
 </vaadin-vertical-layout>
