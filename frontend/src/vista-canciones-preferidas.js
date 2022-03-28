618d7258a4f28ab68b0b8aa124a6bf180719cb1c
Ruth Maria Rodriguez Simon <rrs999@inlumine.ual.es>
2022-03-28 12:42:23 +0200

    Creadas el 70% de las interfaces

--- frontend/src/vista-canciones-preferidas.js
+++ frontend/src/vista-canciones-preferidas.js
@@ -1,24 +1,24 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import './vista-cancion-preferida.js';
 
-/**
- * `vista-canciones-preferidas`
- *
- * VistaCancionesPreferidas element.
- *
- * @customElement
- * @polymer
- */
 class VistaCancionesPreferidas extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-horizontal-layout class="content" style="width: 100%; height: 100%; justify-content: space-around;">
+ <vista-cancion-preferida></vista-cancion-preferida>
+ <vista-cancion-preferida></vista-cancion-preferida>
+ <vista-cancion-preferida></vista-cancion-preferida>
+ <vista-cancion-preferida></vista-cancion-preferida>
+</vaadin-horizontal-layout>
+`;
     }
 
     static get is() {
--- frontend/src/vista-pagina-inicio-administrador.js
+++ frontend/src/vista-pagina-inicio-administrador.js
@@ -1,24 +1,60 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import './vista-cabecera-administrador.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import './vista-canciones-visibles-para-cibernauta.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
-/**
- * `vista-pagina-inicio-administrador`
- *
- * VistaPaginaInicioAdministrador element.
- *
- * @customElement
- * @polymer
- */
 class VistaPaginaInicioAdministrador extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%;">
+ <vista-cabecera-administrador style="width: 100%; height: 10%;"></vista-cabecera-administrador>
+ <h1>Administración</h1>
+ <vaadin-horizontal-layout style="width: 100%;">
+  <vista-canciones-visibles-para-cibernauta style="width: 20%;"></vista-canciones-visibles-para-cibernauta>
+  <vaadin-vertical-layout style="justify-content: space-around; width: 100%;">
+   <vaadin-horizontal-layout theme="spacing" style="width: 100%;">
+    <vaadin-button style="align-self: center; width: 17%;">
+     Solicitudes artistas
+    </vaadin-button>
+    <label style="align-self: center;">Solicitudes enviadas por artistas para unirse a la plataforma.</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="width: 100%;">
+    <vaadin-button style="align-self: center; width: 17%;">
+     Añadir elementos
+    </vaadin-button>
+    <label style="align-self: center;">Adición de artistas, estilos, álbumes y canciones a la base de datos.</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="width: 100%;">
+    <vaadin-button style="align-self: center; width: 17%;">
+     Modificar elementos
+    </vaadin-button>
+    <label style="align-self: center;">Modificación o eliminación de artistas, estilos, álbumes y canciones a la base de datos.</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="width: 100%;">
+    <vaadin-button style="align-self: center; width: 17%;">
+     Vista de usuario/artista
+    </vaadin-button>
+    <label style="align-self: center;">Ver la web con el rol de usuario o artista para probar las acciones que se pueden realizar.</label>
+   </vaadin-horizontal-layout>
+   <vaadin-horizontal-layout theme="spacing" style="justify-content: flex-start; width: 100%;">
+    <vaadin-button style="align-self: center; width: 17%;">
+     Dar de baja usuario
+    </vaadin-button>
+    <label style="align-self: center;">Eliminación de usuario de la plataforma por incumplimiento de las normas.</label>
+   </vaadin-horizontal-layout>
+  </vaadin-vertical-layout>
+ </vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
