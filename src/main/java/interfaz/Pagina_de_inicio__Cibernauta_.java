29504f6c130e5e28f0590d5d051ed68d7227c845
CarlosCarrion98 <c.carrionmartinez1998@gmail.com>
2022-04-18 14:17:59 +0200

    Cambios en el main View

--- src/main/java/interfaz/Pagina_de_inicio__Cibernauta_.java
+++ src/main/java/interfaz/Pagina_de_inicio__Cibernauta_.java
@@ -3,11 +3,16 @@ package interfaz;
 import interfaz.Cibernauta;
 import java.util.Vector;
 import interfaz.Ultimos_exitos;
+import vistas.VistaPaginaInicioCibernauta;
 import interfaz.Tendencias_actuales;
 
-public class Pagina_de_inicio__Cibernauta_ {
+public class Pagina_de_inicio__Cibernauta_ extends VistaPaginaInicioCibernauta{
 //	private Label _texto_contacto;
 	public Cibernauta _unnamed_Cibernauta_;
 	public Vector<Ultimos_exitos> _unnamed_Ultimos_exitos_ = new Vector<Ultimos_exitos>();
 	public Vector<Tendencias_actuales> _unnamed_Tendencias_actuales_ = new Vector<Tendencias_actuales>();
+	
+	public Pagina_de_inicio__Cibernauta_() {
+		
+	}
 }
